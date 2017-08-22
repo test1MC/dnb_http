de19d78b9e89405e70333bbee695fb7d6a5d42ab
dspencer <cbs@cbs.com>
2017-08-22 13:07:25 -0700

    Initial Check-in.

---
+++ Content/remove_old_branding-CONTENT.bat
@@ -0,0 +1,177 @@
+@ECHO RUN THIS IN Content FOLDER
+@del table_style.png.props
+@del 00front_matter\about_this_guide.htm
+@del 00front_matter\copyright_page_x.htm
+@del 00front_matter\Title_dnb360.htm
+@del 00front_matter\Title_dnbi.htm
+@del 00front_matter\Title_dnb_compliance_check.htm
+@del 00front_matter\Title_dnb_customer_integration_manager.htm
+@del 00front_matter\Title_dnb_data_exchange.htm
+@del 00front_matter\Title_dnb_direct.htm
+@del 00front_matter\Title_dnb_family_tree_portal.htm
+@del 00front_matter\Title_dnb_hoovers.htm
+@del 00front_matter\Title_dnb_market_insight.htm
+@del 00front_matter\Title_dnb_optimizer.htm
+@del 00front_matter\Title_dnb_optimizer_online.htm
+@del 00front_matter\Title_docplan.htm
+@del 00front_matter\Title_Template.htm
+@del 00front_matter\TOC.htm
+@del 00front_matter\welcome.htm
+@del 01introduction\ch01_introduction.htm
+@del 01introduction\style_elements.htm
+@del 01introduction\test_topic.htm
+@del 02_more_style_elements\02more_style_elements.htm
+@del 02_more_style_elements\creating_graphics.htm
+@del 02_more_style_elements\creating_tables.htm
+@del 03_setting_up_your_project\02_setting_up_your_project.htm
+@del 03_setting_up_your_project\setting_up_back_matter.htm
+@del 03_setting_up_your_project\setting_up_content_toc.htm
+@del 03_setting_up_your_project\setting_your_pdf_target.htm
+@del 20back_matter\appendix_a_copying_importing_pg_layouts_to_flare.htm
+@del 20back_matter\appendix_b_writing_style_guideline.htm
+@del 20back_matter\back_cover_dnb.htm
+@del 20back_matter\back_cover_hoovers.htm
+@del 20back_matter\glossary.htm
+@del 20back_matter\Index_dnb.htm
+@del 30release_notes\example_rel_notes.htm
+@del Resources\Images\about_this_properties.png
+@del Resources\Images\about_this_properties.png.props
+@del "Resources\Images\appendix properties.png"
+@del Resources\Images\appendix_properties.png
+@del Resources\Images\appendix_properties2.png
+@del Resources\Images\browse_to_images.png
+@del Resources\Images\browse_to_images.png.props
+@del Resources\Images\bubble.png
+@del Resources\Images\bubble.png.props
+@del Resources\Images\bubble_propertes2.png
+@del Resources\Images\bubble_propertes2.png.props
+@del Resources\Images\bubble_properties.png
+@del Resources\Images\bubble_properties.png.props
+@del Resources\Images\bubble_text.png
+@del Resources\Images\bubble_text.png.props
+@del Resources\Images\center_bubble_text.png
+@del Resources\Images\center_bubble_text.png.props
+@del Resources\Images\chapter_properties.png
+@del Resources\Images\chapter_properties.png.props
+@del Resources\Images\chapter_properties2.png
+@del Resources\Images\chapter_properties_2.png
+@del Resources\Images\copyright_page_propeties.png
+@del Resources\Images\copyright_page_propeties.png.props
+@del Resources\Images\ctrl_panel_fonts.png
+@del Resources\Images\ctrl_panel_fonts.png.props
+@del "Resources\Images\DB-3520 User Guide Template- DNB CIM.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-BACK COVER_Header.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNB Family Tree Portal.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNB Market Insight.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNB Optimizer Online.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNB Optimizer.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNB360.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNBComplianceCheck.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNBDataExchange V4.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNBDataExchange.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNBDataExchange.jpg.props"
+@del "Resources\Images\DB-3520 User Guide Template-DNBDIRECT.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-DNBi.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-Hoovers.jpg"
+@del "Resources\Images\DB-3520 User Guide Template-Hoovers_June 13.jpg"
+@del Resources\Images\div_avoid_break.png
+@del Resources\Images\div_avoid_break.png.props
+@del Resources\Images\dnb360_top_menu.png.props
+@del Resources\Images\DNB_corp30_pos.png
+@del Resources\Images\DNB_Cover_Graphic.jpg
+@del Resources\Images\dnb_letterhead_banner-CustomerIntegrationManager.jpg
+@del Resources\Images\dnb_letterhead_banner.png
+@del Resources\Images\dnb_letterhead_banner_DNB360.jpg
+@del Resources\Images\dnb_letterhead_banner_DNBCOMPLAINCECHECK.jpg
+@del Resources\Images\dnb_letterhead_banner_DNBDATAEXCHANGE.jpg
+@del Resources\Images\dnb_letterhead_banner_DNBDIRECT.jpg
+@del Resources\Images\dnb_letterhead_banner_DNBi.jpg
+@del Resources\Images\dnb_letterhead_banner_FamilyTreePortal.jpg
+@del Resources\Images\dnb_letterhead_banner_MarketInsight.jpg
+@del Resources\Images\dnb_letterhead_banner_Optimizer.jpg
+@del Resources\Images\dnb_letterhead_banner_OptimizerOnline.jpg
+@del Resources\Images\dnb_letterhead_lower_border.png
+@del Resources\Images\dnb_logo_footer.png
+@del Resources\Images\download_font.png
+@del Resources\Images\edit_titlepage_image.png
+@del Resources\Images\example_content_toc.png
+@del Resources\Images\example_content_toc.png.props
+@del Resources\Images\file_import_step1.png
+@del Resources\Images\file_import_step1.png.props
+@del Resources\Images\flare_project_folder1.png
+@del Resources\Images\flare_project_folder1.png.props
+@del Resources\Images\flare_project_folder2.png
+@del Resources\Images\flare_project_folder2.png.props
+@del Resources\Images\glossary_properties.png
+@del Resources\Images\graphic_example.png
+@del Resources\Images\graphic_example.png.props
+@del Resources\Images\header_rows.png
+@del Resources\Images\hoovers_rgb_blue.jpg
+@del Resources\Images\hoovers_rgb_blue.jpg.props
+@del Resources\Images\index_page_properties.png
+@del Resources\Images\index_page_properties2.png
+@del Resources\Images\index_properties.png
+@del Resources\Images\index_properties2.png
+@del Resources\Images\index_properties2.png.props
+@del Resources\Images\master_stylesheet.png
+@del Resources\Images\mini_toc_proxy.png
+@del Resources\Images\my_pallette.png
+@del Resources\Images\other_topics_properties.png
+@del Resources\Images\pdf_target_properties.png
+@del Resources\Images\pointer.png
+@del Resources\Images\reset_image.png
+@del Resources\Images\rns_properties.png
+@del Resources\Images\rns_properties_2.png
+@del Resources\Images\rns_properties_3.png
+@del Resources\Images\save_fonts.png
+@del Resources\Images\save_to_pallette.png
+@del Resources\Images\save_to_pallette.png.props
+@del Resources\Images\select_file_format_import_file.png
+@del Resources\Images\select_letterhead_image.png
+@del Resources\Images\select_pg_layout_header_frame.png
+@del Resources\Images\select_pg_layout_header_frame.png.props
+@del Resources\Images\set_bubble.png
+@del Resources\Images\set_target_pdf_settings.png
+@del Resources\Images\set_target_pdf_settings.png.props
+@del Resources\Images\set_target_pdf_stylesheet.png
+@del Resources\Images\set_target_pdf_stylesheet.png.props
+@del Resources\Images\subsequent_chapter_numbering.png
+@del Resources\Images\subsequent_chapter_numbering.png.props
+@del Resources\Images\table_properties.png
+@del Resources\Images\table_properties.png.props
+@del Resources\Images\table_properties2.png
+@del Resources\Images\table_properties2.png.props
+@del Resources\Images\table_style.png
+@del Resources\Images\table_style.png.props
+@del Resources\Images\title_page_properties1.png
+@del Resources\Images\title_page_properties1.png.props
+@del Resources\Images\title_page_properties2.png
+@del Resources\Images\title_page_properties2.png.props
+@del Resources\Images\toc_properties.png
+@del Resources\Images\toc_properties.png.props
+@del Resources\MasterPages\dnb360_masterpage.flmsp
+@del Resources\PageLayouts\BackCover.flpgl
+@del Resources\PageLayouts\Chapter.flpgl
+@del Resources\PageLayouts\copyright.flpgl
+@del Resources\PageLayouts\Index.flpgl
+@del Resources\PageLayouts\ReleaseNotes.flpgl
+@del Resources\PageLayouts\TitlePage.flpgl
+@del Resources\Snippets\or_after_alpha_step.flsnp
+@del Resources\Snippets\or_after_numbered_step.flsnp
+@del Resources\Stylesheets\dnbStyles.css
+@del Resources\TableStyles\dnb_360_step_table.css
+@del Resources\TableStyles\dnb_footer_table.css
+@del Resources\TableStyles\dnb_header_table.css
+@del Resources\TableStyles\dnb_tablestyle_1.css
+@del Resources\TableStyles\dnb_tablestyle_2.css
+@del Resources\TableStyles\dnb_tablestyle_3.css
+@del Resources\TableStyles\intro_table.css
+@del Resources\TableStyles\top_aligned.css
+@rmdir 00front_matter
+@rmdir 01introduction
+@rmdir 02_more_style_elements
+@rmdir 03_setting_up_your_project
+@rmdir 20back_matter
+@rmdir 30release_notes
+@del remove_old_branding-CONTENT.bat
+@ECHO Process completed!
---
+++ Project/remove_old_branding-PROJECT.bat
@@ -0,0 +1,28 @@
+@ECHO RUN THIS IN Project FOLDER
+@del Advanced\dnb360_ui_csh_alias_file.flali
+@del Advanced\dnb360_ui_mapping.h
+@del Advanced\English (United States) Dictionary.mcdic
+@del Advanced\MyAuthorFileTagSet.flfts
+@del Advanced\MyHeaderFile.h
+@del Advanced\MyStatusFileTagSet.flfts
+@del Advanced\my_alias_file.flali
+@del Destinations\MyDestination.fldes
+@del Glossaries\MyGlossary.flglo
+@del Reports\dnb_page_layouts_print - Monday, June 17, 2013 [9.19 AM].mclog
+@del Reports\dnb_page_layouts_print - Thursday, June 13, 2013 [1.49 PM].mclog
+@del Skins\DefaultHTML5.flskn
+@del Skins\dnb360_skin.flskn
+@del Targets\dnb360_release_notes.fltar
+@del Targets\dnb_master_project_print.fltar
+@del Targets\MyPDFOutput.fltar
+@del TOCs\dnb360_release_notes.fltoc
+@del TOCs\dnb_master_project_print.fltoc
+@del Users\ArnoldA\RecentImages.xml
+@del Users\ArnoldA\RecentLinks.xml
+@del Users\ArnoldA\RecentSnippets.xml
+@del VariableSets\DNBVariableSet.flvar
+@del VariableSets\MyVariables.flvar
+@del VariableSets\NewVariableSet.flvar
+@rmdir Users\ArnoldA
+@del remove_old_branding-PROJECT.bat
+@ECHO Process completed!
